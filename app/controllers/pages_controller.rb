918d0f8541aa358d01db7592ca594a639f8eda4c
洪璽凱 <tpanakin@gmail.com>
2017-07-30 05:36:59 +0800

    add team page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,16 @@
 class PagesController < ApplicationController
   def home
-
   end
 
   def mentor
-
   end
 
   def index
+  end
 
+  def plan
+  end
+
+  def team
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-  get '/mentors', to: 'pages#mentor'
+  get '/plan', to: 'pages#plan'
+  get '/mentor', to: 'pages#mentor'
+  get '/team', to: 'pages#team'
   get '/home', to: 'pages#home'
 
 end
